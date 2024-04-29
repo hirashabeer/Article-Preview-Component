6774a459aacf26c4123b84671e007596730d082d
hirashabeer <148793818+hirashabeer@users.noreply.github.com>
2024-04-29 14:00:27 +0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,22 @@
+const popup = document.getElementById("popup");
+
+const sharebtn = document.getElementById("share");
+const sharebtn2 = document.getElementById("share2");
+ 
+sharebtn.addEventListener("click", () => {
+  popup.classList.toggle("svg");
+ 
+  
+ 
+});
+sharebtn2.addEventListener("click", () => {
+    popup.classList.toggle("svg");
+   
+    
+   
+  });
+
+
+
+
+
